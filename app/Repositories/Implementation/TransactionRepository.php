1dc398f7afc09680b130251fbac8f0453e98ccf6
yamdev07 <yamdyamd080@gmail.com>
2026-01-07 18:10:52 +0100

    add search by name

--- app/Repositories/Implementation/TransactionRepository.php
+++ app/Repositories/Implementation/TransactionRepository.php
@@ -26,20 +26,40 @@ class TransactionRepository implements TransactionRepositoryInterface
     {
         return Transaction::with('user', 'room', 'customer')
             ->where('check_out', '>=', Carbon::now())
-            ->when(! empty($request->search), function ($query) use ($request) {
-                $query->where('id', '=', $request->search);
+            ->when($request->filled('search'), function ($query) use ($request) {
+                $search = $request->search;
+
+                $query->where(function ($q) use ($search) {
+                    $q->where('transactions.id', 'like', "%{$search}%")
+                    ->orWhereHas('customer', function ($c) use ($search) {
+                        $c->where('customers.name', 'like', "%{$search}%");
+                    });
+                });
             })
-            ->orderBy('check_out', 'ASC')->orderBy('id', 'DESC')->paginate(20)
+            ->orderBy('check_out', 'ASC')
+            ->orderBy('id', 'DESC')
+            ->paginate(20)
             ->appends($request->all());
     }
 
+
     public function getTransactionExpired($request)
     {
-        return Transaction::with('user', 'room', 'customer')->where('check_out', '<', Carbon::now())
-            ->when(! empty($request->search), function ($query) use ($request) {
-                $query->where('id', '=', $request->search);
+        return Transaction::with('user', 'room', 'customer')
+            ->where('check_out', '<', Carbon::now())
+            ->when($request->filled('search'), function ($query) use ($request) {
+                $search = $request->search;
+
+                $query->where(function ($q) use ($search) {
+                    $q->where('transactions.id', 'like', "%{$search}%")
+                    ->orWhereHas('customer', function ($c) use ($search) {
+                        $c->where('customers.name', 'like', "%{$search}%");
+                    });
+                });
             })
-            ->orderBy('check_out', 'ASC')->paginate(20)
+            ->orderBy('check_out', 'ASC')
+            ->paginate(20)
             ->appends($request->all());
     }
+
 }
