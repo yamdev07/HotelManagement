86aadae25c3d311815067c28b5830e22a964b3ee
judepavel <judepavel7@gmail.com>
2026-07-23 17:41:09 +0100

    fix: remove unused import in StaffCreatedNotification

--- app/Notifications/StaffCreatedNotification.php
+++ app/Notifications/StaffCreatedNotification.php
@@ -4,7 +4,6 @@ namespace App\Notifications;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 /**
